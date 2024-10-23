ea5ae864267813a89543eb0f3aef3ccf86e26d4d
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-23 05:33:06 +0100

    Refactor constants file to remove unused environment variables

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -37,10 +37,6 @@ export const EMAIL_HOST = process.env.EMAIL_HOST as string,
     SUPER_ADMIN_EMAIL1 = process.env.SUPER_ADMIN_EMAIL1 as string;
 
 
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY as string;
-
-export const POSTMARK_API_KEY = process.env.POSTMARK_API_KEY as string;
-
 export const WEBSITE_URL = NODE_ENV === 'production' ? process.env.WEBSITE_URL as string : 'http://localhost:5173';
 
 export const DOCUMENTATION_URL = process.env.DOCUMENTATION_URL as string;
@@ -51,26 +47,6 @@ export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY as string;
 
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME as string;
 
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
-
-export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
-
-export const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID as string;
-
-export const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET as string;
-
-export const API_URL = process.env.API_URL as string;
-
-export const SESSION_SECRET = process.env.SESSION_SECRET as string;
-
 export const ADMIN_EMAIL = process.env.ADMIN_EMAIL as string;
 
-export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
-
-export const STRIPE_API_KEY = process.env.STRIPE_API_KEY as string;
-
-export const ZOOM_ACCOUNT_ID = process.env.ZOOM_ACCOUNT_ID as string;
-
-export const ZOOM_CLIENT_ID = process.env.ZOOM_CLIENT_ID as string;
-
-export const ZOOM_CLIENT_SECRET = process.env.ZOOM_CLIENT_SECRET as string;
+export const SIGNATURE_MESSAGE = process.env.SIGNATURE_MESSAGE as string;
