a2eada2324490f73ebe6974e09d8f9c0d13c3ff9
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-24 14:44:49 +0100

    Refactor property.routes.ts to include file uploads for properties

--- src/routes/property.routes.ts
+++ src/routes/property.routes.ts
@@ -1,14 +1,24 @@
 import express, { Router } from 'express';
 import PropertyController from '../controllers/property.controller';
 import { AuthenticatedController, basicAuth } from '../middlewares/authMiddleware';
+import { uploadMiddleware, UploadType } from '../middlewares/uploadMiddleware';
 
 const router: Router = express.Router();
+
+// Configure the upload fields for properties
+const propertyUploadFields = [
+    { name: 'banner', maxCount: 1 },
+    { name: 'gallery', maxCount: 4 },
+    { name: 'doc', maxCount: 3 },
+];
+
+const upload = uploadMiddleware(UploadType.Fields, propertyUploadFields);
 
 router
     .get('/', PropertyController.getAllProperties)
     .get('/:id', PropertyController.getPropertyById)
     .post('/', basicAuth(), AuthenticatedController(PropertyController.addProperty))
-    .patch('/:id', basicAuth(), AuthenticatedController(PropertyController.updateProperty))
+    .patch('/:id', basicAuth(), upload, AuthenticatedController(PropertyController.updateProperty))
     .get('/owner/stats', basicAuth(), AuthenticatedController(PropertyController.getOwnerStats))
     .delete('/:id', basicAuth(), AuthenticatedController(PropertyController.deleteProperty));
 
