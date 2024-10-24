3c717cd8863ccf6868dcdab207aeeb1cbef63bb8
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-24 02:19:34 +0100

    Refactor PropertyStats model to include tokenomics and update data types

---
+++ src/models/tokenomics.model.ts
@@ -0,0 +1,52 @@
+import { Table, Column, Model, DataType, ForeignKey, BelongsTo, IsUUID } from 'sequelize-typescript';
+import Property from './property.model';
+
+@Table
+export default class Tokenomics extends Model<Tokenomics> {
+    // @IsUUID(4)
+    // @PrimaryKey
+    // @Default(DataType.UUIDV4)
+    // @Column
+    // id: string;
+
+    @ForeignKey(() => Property)
+    @IsUUID(4)
+    @Column({ type: DataType.STRING, primaryKey: true })
+        propertyId: string;
+
+    @Column(DataType.INTEGER)
+        totalTokenSupply: number;
+
+    @Column(DataType.INTEGER)
+        remainingTokens: number;
+
+    @Column(DataType.DECIMAL(10, 2))
+        tokenPrice: number;
+
+    @Column(DataType.JSONB)
+        distribution: {
+        team: number;
+        advisors: number;
+        investors: number;
+        other: number;
+    };
+
+    @Column(DataType.TEXT)
+        distributionDescription: string;
+
+    @BelongsTo(() => Property, 'propertyId')
+        property: Property;
+}
+
+export interface ITokenomics {
+    propertyId: string;
+    totalTokenSupply: number;
+    tokenPrice: number;
+    distribution: {
+        team: number;
+        advisors: number;
+        investors: number;
+        other: number;
+    };
+    distributionDescription: string;
+}
