4bb8becb55f1c5d5c5646112c9e21e397ea664eb
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-26 06:04:31 +0100

    Refactor blog.routes.ts to add document upload functionality and update authentication middleware

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -2,24 +2,29 @@
 // blog.routes.ts
 import express, { Router } from 'express';
 import BlogController from '../controllers/blog.controller';
-import { adminAuth, AuthenticatedController, basicAuth } from '../middlewares/authMiddleware';
+import { AuthenticatedController, basicAuth } from '../middlewares/authMiddleware';
 import { uploadMiddleware, UploadType } from '../middlewares/uploadMiddleware';
 
 const router: Router = express.Router();
 
-const upload = uploadMiddleware(UploadType.Array, 'media', 5);
+const propertyUploadFields = [
+    { name: 'images', maxCount: 5 },
+    { name: 'documents', maxCount: 1 },
+];
+
+const upload = uploadMiddleware(UploadType.Fields, propertyUploadFields);
 
 router.route('/')
     .get(BlogController.getAllBlogs)
     .post(
-        adminAuth('admin'),
+        basicAuth(),
         upload,
         AuthenticatedController(BlogController.createBlog)
     );
 
 router.route('/')
     .patch(
-        adminAuth('admin'),
+        basicAuth(),
         upload,
         AuthenticatedController(BlogController.updateBlog)
     )
@@ -27,7 +32,7 @@ router.route('/')
 
 router.get('/info', BlogController.getBlog);    
 router.post('/engage', basicAuth(), AuthenticatedController(BlogController.engageWithBlog));
-router.delete('/blog-comment', adminAuth('admin'), AuthenticatedController(BlogController.deleteActivityComment));
+router.delete('/blog-comment', basicAuth(), AuthenticatedController(BlogController.deleteActivityComment));
 router.get('/activities', BlogController.getBlogActivities);
 
 export default router;
