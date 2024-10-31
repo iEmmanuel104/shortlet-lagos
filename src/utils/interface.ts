bd7d2e3a62b29d140c015933914bd80a44d629ea
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-31 04:03:25 +0100

    Refactor interface.ts to add new interfaces for investor statistics, investment metrics, and property owner stats

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import User, { IUser } from '../models/user.model';
 
 export interface SaveTokenToCache {
@@ -57,4 +58,129 @@ export interface AWSKeyData {
     id: string,
     fileName: string,
     type: AWSUploadType,
+}
+
+export interface IInvestorStats {
+    totalInvestments: number;
+    totalInvestedAmount: number;
+    accountValue: number;
+    valueChange: {
+        amount: number;
+        percentage: number;
+    };
+    investments: {
+        processing: {
+            count: number;
+            amount: number;
+        };
+        completed: {
+            count: number;
+            amount: number;
+        };
+    };
+    rentals: {
+        balance: number;
+        totalEarned: number;
+        pendingPayouts: number;
+    };
+    portfolio: {
+        totalPropertyValue: number;
+        valueChange: {
+            amount: number;
+            percentage: number;
+        };
+    };
+}
+
+export interface IInvestmentMetrics {
+    period: string;
+    investedAmount: number;
+    propertyValue: number;
+    rentalIncome: number;
+}
+
+export enum MetricsPeriod {
+    DAY = 'day',
+    WEEK = 'week',
+    MONTH = 'month',
+    SIXMONTH = 'sixmonth',
+    YEAR = 'year',
+    FIVEYEAR = 'fiveyear'
+}
+
+export interface ITopInvestment {
+    propertyId: string;
+    propertyName: string;
+    location: string;
+    investedAmount: number;
+    currentValue: number;
+    valueChange: {
+        amount: number;
+        percentage: number;
+    };
+    rentalYield: number;
+    investmentDate: Date;
+}
+
+export interface IPropertyOwnerStats {
+    totalListings: number;
+    activeListings: number;
+    totalInvestmentAmount: number;
+    totalInvestorsCount: number;
+    investments: {
+        completed: {
+            count: number;
+            percentageChange: number;
+        };
+        pending: {
+            count: number;
+            percentageChange: number;
+        };
+    };
+    recentActivity: {
+        newInvestors: number;
+        newInvestments: number;
+    };
+}
+
+export enum TimePeriod {
+    DAY = 'day',
+    WEEK = 'week',
+    MONTH = 'month'
+}
+
+// Stats data point interface
+export interface TimeBasedStats {
+    period: string;
+    investmentAmount: number;
+    investorCount: number;
+}
+
+// Time series data interface
+export interface TimeSeriesData {
+    period: TimePeriod;
+    data: TimeBasedStats[];
+}
+
+// Extended stats interface
+export interface IPropertyOwnerStatsWithTimeSeries extends IPropertyOwnerStats {
+    timeSeriesData?: TimeSeriesData;
+}
+
+export interface ITopPropertyInvestment {
+    id: string;
+    name: string;
+    location: string;
+    banner: string;
+    metrics: {
+        MIA: number; // Minimum Investment Amount
+    };
+    stats: {
+        totalInvestmentAmount: number;
+        numberOfInvestors: number;
+    };
+    investmentTrend: {
+        period: string;
+        amount: number;
+    }[];
 }
