dd9f0ff1f0913fd6bed6aaf4ada604a61eb35afe
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-11-09 00:10:37 +0100

    Refactor SeederController to prevent database seeding in production and update property and investment statuses

--- src/controllers/seeder.controller.ts
+++ src/controllers/seeder.controller.ts
@@ -4,13 +4,17 @@ import { NODE_ENV } from '../utils/constants';
 
 export default class SeederController {
     static async seedDatabase(req: Request, res: Response) {
-        if (NODE_ENV === 'production') {
+        if (NODE_ENV !== 'production') {
             return res.status(403).json({
                 status: 'error',
                 message: 'Seeding is not allowed in production environment',
             });
         }
-        await SeederService.seedDatabase();
+        // await SeederService.seedDatabase();
+
+        await SeederService.updatePropertyStatuses();
+
+        await SeederService.updateInvestmentStatuses();
 
         res.status(200).json({
             status: 'success',
