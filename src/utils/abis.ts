da442eb50d2679e904f73feb2c90e12c38a3dcb1
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-11-09 15:12:33 +0100

    Add ABIs and contract addresses for USDC and Real Estate Factory

---
+++ src/utils/abis.ts
@@ -0,0 +1,150 @@
+export const FACTORY_CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3' as const;
+export const BASE_USDC_CONTRACT_ADDRESS = '0x9A676e781A523b5d0C0e437313B3732f905b5206' as const;
+export const PRIVATE_KEY = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+
+export const USDC_ABI = [
+    {
+        constant: true,
+        inputs: [],
+        name: 'name',
+        outputs: [{ name: '', type: 'string' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: false,
+        inputs: [
+            { name: 'spender', type: 'address' },
+            { name: 'value', type: 'uint256' },
+        ],
+        name: 'approve',
+        outputs: [{ name: '', type: 'bool' }],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: 'totalSupply',
+        outputs: [{ name: '', type: 'uint256' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: false,
+        inputs: [
+            { name: 'from', type: 'address' },
+            { name: 'to', type: 'address' },
+            { name: 'value', type: 'uint256' },
+        ],
+        name: 'transferFrom',
+        outputs: [{ name: '', type: 'bool' }],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: 'decimals',
+        outputs: [{ name: '', type: 'uint8' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [{ name: 'owner', type: 'address' }],
+        name: 'balanceOf',
+        outputs: [{ name: 'balance', type: 'uint256' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: 'symbol',
+        outputs: [{ name: '', type: 'string' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: false,
+        inputs: [
+            { name: 'to', type: 'address' },
+            { name: 'value', type: 'uint256' },
+        ],
+        name: 'transfer',
+        outputs: [{ name: '', type: 'bool' }],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [
+            { name: 'owner', type: 'address' },
+            { name: 'spender', type: 'address' },
+        ],
+        name: 'allowance',
+        outputs: [{ name: '', type: 'uint256' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        anonymous: false,
+        inputs: [
+            { indexed: true, name: 'owner', type: 'address' },
+            { indexed: true, name: 'spender', type: 'address' },
+            { indexed: false, name: 'value', type: 'uint256' },
+        ],
+        name: 'Approval',
+        type: 'event',
+    },
+    {
+        anonymous: false,
+        inputs: [
+            { indexed: true, name: 'from', type: 'address' },
+            { indexed: true, name: 'to', type: 'address' },
+            { indexed: false, name: 'value', type: 'uint256' },
+        ],
+        name: 'Transfer',
+        type: 'event',
+    },
+] as const;
+
+export const REAL_ESTATE_FACTORY_ABI = [
+    {
+        inputs: [
+            { type: 'string', name: 'name' },
+            { type: 'string', name: 'symbol' },
+            { type: 'uint256', name: 'initialAssetValue' },
+            { type: 'uint256', name: '_maxSupply' },
+            { type: 'address', name: 'ownerAddress' }
+        ],
+        name: 'createRealEstateToken',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function'
+    },
+    {
+        anonymous: false,
+        inputs: [
+            { indexed: true, name: 'newTokenAddress', type: 'address' },
+            { indexed: false, name: 'name', type: 'string' },
+            { indexed: false, name: 'symbol', type: 'string' },
+            { indexed: false, name: 'initialAssetValue', type: 'uint256' },
+            { indexed: false, name: 'maxSupply', type: 'uint256' },
+            { indexed: true, name: 'owner', type: 'address' }
+        ],
+        name: 'RealEstateTokenCreated',
+        type: 'event'
+    }
+] as const;
