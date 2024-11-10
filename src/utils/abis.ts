30915a343eb2717330100f32078eb1f0db74474e
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-11-10 12:40:33 +0100

    Update contract addresses and ABI definitions for consistency and security

--- src/utils/abis.ts
+++ src/utils/abis.ts
@@ -1,6 +1,6 @@
-export const FACTORY_CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3' as const;
-export const BASE_USDC_CONTRACT_ADDRESS = '0x9A676e781A523b5d0C0e437313B3732f905b5206' as const;
-export const PRIVATE_KEY = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const FACTORY_CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const BASE_USDC_CONTRACT_ADDRESS = '0x9A676e781A523b5d0C0e437313B3732f905b5206';
+export const PRIVATE_KEY = '0x7289fd08ecb07ef5066c7829f48ed4a540a79913e599b2a98db2b8f537d5e2bc';
 
 
 export const USDC_ABI = [
@@ -127,12 +127,12 @@ export const REAL_ESTATE_FACTORY_ABI = [
             { type: 'string', name: 'symbol' },
             { type: 'uint256', name: 'initialAssetValue' },
             { type: 'uint256', name: '_maxSupply' },
-            { type: 'address', name: 'ownerAddress' }
+            { type: 'address', name: 'ownerAddress' },
         ],
         name: 'createRealEstateToken',
         outputs: [],
         stateMutability: 'nonpayable',
-        type: 'function'
+        type: 'function',
     },
     {
         anonymous: false,
@@ -142,9 +142,9 @@ export const REAL_ESTATE_FACTORY_ABI = [
             { indexed: false, name: 'symbol', type: 'string' },
             { indexed: false, name: 'initialAssetValue', type: 'uint256' },
             { indexed: false, name: 'maxSupply', type: 'uint256' },
-            { indexed: true, name: 'owner', type: 'address' }
+            { indexed: true, name: 'owner', type: 'address' },
         ],
         name: 'RealEstateTokenCreated',
-        type: 'event'
-    }
+        type: 'event',
+    },
 ] as const;
