5c315caf41374c4b90c9105f4ccd272300583838
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-11-30 20:35:25 +0100

    Update user routes to use optionalAuth middleware for getUser and updateUser endpoints

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 import UserController from '../controllers/user.controller';
-import { AuthenticatedController, basicAuth } from '../middlewares/authMiddleware';
+import { AuthenticatedController, basicAuth, optionalAuth } from '../middlewares/authMiddleware';
 import { uploadMiddleware, UploadType } from '../middlewares/uploadMiddleware';
 
 const router: Router = express.Router();
@@ -11,8 +11,8 @@ const upload = uploadMiddleware(UploadType.Single, 'file');
 router
     .post('/support-ticket', basicAuth(), AuthenticatedController(UserController.createSupportTicket))
     .get('/', UserController.getAllUsers)
-    .get('/info', basicAuth(), AuthenticatedController(UserController.getUser))
-    .patch('/update', basicAuth(), upload, AuthenticatedController(UserController.updateUser));
+    .get('/info', optionalAuth, AuthenticatedController(UserController.getUser))
+    .patch('/update', optionalAuth, upload, AuthenticatedController(UserController.updateUser));
     
 export default router;
 
